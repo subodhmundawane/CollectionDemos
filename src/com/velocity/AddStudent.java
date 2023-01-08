2f13b986ab372be66e73c10d2975297e382684b8
subodhmundawane <subodhmundawane123@gmail.com>
2023-01-08 15:17:56 +0530

    code for arraylist

---
+++ src/com/velocity/AddStudent.java
@@ -0,0 +1,22 @@
+package com.velocity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddStudent {
+	public static void main(String[] args) {
+		List<String> list=new ArrayList<String>();
+		
+		list.add("Manish");
+		list.add("Subodh");
+		list.add("Sandip");
+		list.add("Shubham");
+		list.add("Suraj");
+		System.out.println("List of Student>>" + list);
+		
+		
+		
+	}
+	
+
+}
